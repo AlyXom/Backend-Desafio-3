c7d8f11537d92b047332aa07d2640a74d06ccdb8
Alysson <alyssoncraft12@gmail.com>
2024-10-16 18:54:05 -0300

    feat/categories resource

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoryEntity } from './categories.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryController } from './categories.controller';
 
 @Module({
-  imports: [CategoryEntity],
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  controllers: [CategoryController],
   providers: [CategoriesService],
-  exports: [CategoryEntity]
+  exports: [TypeOrmModule]
 })
 export class CategoriesModule {}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './categories.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+
+    constructor(@InjectRepository(CategoryEntity) private readonly categoryRepository: Repository<CategoryEntity>) {}
+
+    async getAllCategories() {
+        return this.categoryRepository.find()
+    }
+}
